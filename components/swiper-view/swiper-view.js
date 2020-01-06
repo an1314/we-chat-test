1a983a7b52fa903e994f2f61f612ca9598b31936
unknown <unknown@example.com>
2020-01-06 23:23:10 +0800

    Initial Commit

---
+++ components/swiper-view/swiper-view.js
@@ -0,0 +1,26 @@
+// components/swiper-view/swiper-view.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    autoplay: false,
+    duration: 500,
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
