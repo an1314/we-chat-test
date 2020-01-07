d1d7864fdfd31659dad823a1a8a6bd93fa432654
an1314ybz <1293033653@qq.com>
2020-01-08 00:41:51 +0800

    添加滑动图片组件

---
+++ components/img-scroll/img-scroll.js
@@ -0,0 +1,42 @@
+// components/img-scroll/img-scroll.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    images: {
+      type: Array,
+      value: [{
+        img: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+        url: '',
+        
+      },{
+        img: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+        url: '',
+        
+      },{
+        img: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+        url: '',
+        
+      },{
+        img: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
+        url: '',
+        
+      }]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
