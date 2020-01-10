17bf25ab92922603e03b7636071b5211008903c3
liuchong <chong.liu@ebestmobile.com>
2020-01-10 18:47:48 +0800

    aa

---
+++ components/product-card/product-card.js
@@ -0,0 +1,31 @@
+// components/product-card/product-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    data: {
+      type: Object,
+      value: {
+        label: '测试产品',
+        desc: '避风塘酱酿翅',
+        price: 43.0,
+        url: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
